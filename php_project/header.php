6fe98a14ed9ecbcf5a167ef2749f1c8f91eec5d5
runadkhamkar <74759219+runadkhamkar@users.noreply.github.com>
2021-08-02 21:10:22 +0530

    Add files via upload

---
+++ php_project/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INDEX PAGE</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+<nav>
+	<div class="main-wrapper">
+		<ul>
+		<li><a href="index.php">HOME</a></li>	
+		</ul>
+		<div class="nav-login">
+			<form>
+				<input type="text" name="uid" placeholder="Username/e-mail">
+				<input type="password" name="pwd" placeholder="password">
+				<button type="submit" name="submit">Login</button>
+			</form>
+			<a href="signup.php">Sign up</a>
+		</div>
+	</div>
+</nav>	
+</header>
